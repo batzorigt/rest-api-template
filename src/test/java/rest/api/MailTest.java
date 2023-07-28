bea68eab751b21aaa1fac1c4b758aac7842a7b6a
b-rentsen <b-rentsen@gmofh.com>
2023-07-28 17:16:09 +0900

    added comments

--- src/test/java/rest/api/MailTest.java
+++ src/test/java/rest/api/MailTest.java
@@ -3,8 +3,6 @@ package rest.api;
 import java.io.File;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
-
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.AddressException;
 
@@ -15,12 +13,12 @@ public class MailTest {
 
     List<String> to = List.of("recipient1@mail.address", "recipient2@mail.address");
 
-    @Test
+    // @Test TODO uncomment to test
     void sendTextMail() throws AddressException, MessagingException {
         Mail.send(to, null, null, "text mail", "This is test!");
     }
 
-    @Test
+    // @Test TODO uncomment to test
     void sendMailWithAttachment() throws AddressException, MessagingException {
         Mail.send(to, null, null, "text mail with attachment", "I sent you query.sql!", new File(
                 "./src/test/resources/query.sql"));
