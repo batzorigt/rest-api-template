1ce8da149f1cf2ddfd79f3c67d32db9a1fdca5cb
BatzorigtRentsen <BatzorigtRentsen@MIRAI-DEV2-09>
2026-05-13 13:10:55 +0800

    refactor: Remove unused Metrics import and bind metrics directly to the Prometheus registry

--- src/main/java/rest/api/Micrometer.java
+++ src/main/java/rest/api/Micrometer.java
@@ -4,7 +4,6 @@ import org.jspecify.annotations.NonNull;
 
 import io.javalin.config.JavalinConfig;
 import io.javalin.micrometer.MicrometerPlugin;
-import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.binder.jvm.JvmCompilationMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
@@ -19,34 +18,34 @@ import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
 public class Micrometer {
 
+	@NonNull
     private final PrometheusMeterRegistry registry = new PrometheusMeterRegistry(new PrometheusConfig() {
 
-        @Override
-        public String get(String key) {
-            return null;
-        }
+		@Override
+	    public String get(@NonNull String key) {
+	        return null;
+	    }
 
-        @Override
-        public @NonNull String prefix() {
-            return "rapit";
-        }
+	    @Override
+	    public @NonNull String prefix() {
+	        return "rapit";
+	    }
 
-    });
-
+	});
+    		
     @SuppressWarnings("resource")
     public Micrometer(JavalinConfig config) {
-        Metrics.addRegistry(registry);
         registry.config().commonTags("application", "rapit");
 
-        new JvmGcMetrics().bindTo(Metrics.globalRegistry);
-        new JvmHeapPressureMetrics().bindTo(Metrics.globalRegistry);
-        new JvmThreadMetrics().bindTo(Metrics.globalRegistry);
-        new JvmCompilationMetrics().bindTo(Metrics.globalRegistry);
-        new JvmMemoryMetrics().bindTo(Metrics.globalRegistry);
-        new Log4j2Metrics().bindTo(Metrics.globalRegistry);
-        new UptimeMetrics().bindTo(Metrics.globalRegistry);
-        new FileDescriptorMetrics().bindTo(Metrics.globalRegistry);
-        new ProcessorMetrics().bindTo(Metrics.globalRegistry);
+        new JvmGcMetrics().bindTo(registry);
+        new JvmHeapPressureMetrics().bindTo(registry);
+        new JvmThreadMetrics().bindTo(registry);
+        new JvmCompilationMetrics().bindTo(registry);
+        new JvmMemoryMetrics().bindTo(registry);
+        new Log4j2Metrics().bindTo(registry);
+        new UptimeMetrics().bindTo(registry);
+        new FileDescriptorMetrics().bindTo(registry);
+        new ProcessorMetrics().bindTo(registry);
         // new DiskSpaceMetrics(new
         // File(System.getProperty("user.dir"))).bindTo(registry);
 
